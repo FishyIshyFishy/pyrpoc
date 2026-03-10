b8f789b46305a92081cd11584498d19537b615ea
Ishaan Singh <sing1125@purdue.edu>
2026-03-10 16:19:25 -0400

    clean repo

--- data/swabian_raw_flim/npzreader.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-data = np.load(r'C:\Users\ishaa\Documents\ZhangLab\python\new_pysrs\data\swabian_raw_flim\20260304_123247_flim_test_001_raw.npz',
-               allow_pickle=True)
-print(data.files)
-frames = data['frames']
-print(frames.shape)
